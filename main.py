6d1db4155c80bae8e4fed78cbb6b7594b07d9077
mjbramm <103207134+mjbramm@users.noreply.github.com>
2022-09-28 12:18:24 -0600

    Edit B4 Class

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 # Tanner Atkinson 
-<<<<<<< HEAD
-
-
-
-
-=======
 # Mitch Brammer
-<<<<<<< HEAD
 # Drew Goldston
-=======
->>>>>>> a49b9c88a9f55ce9dc3c3dd576b9c47464d1d912
->>>>>>> f4bf51c89b6c5261258391c3fa6fb2dc33763ebe
