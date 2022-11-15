1cf0858e22a19395f822b7556aa59e6656193f00
Tannerka5 <38678080+Tannerka5@users.noreply.github.com>
2022-11-15 13:05:55 -0700

    Creating learn more page

--- missingpersons/missingpersonsapp/urls.py
+++ missingpersons/missingpersonsapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import indexPageView, dataPageView
+from .views import indexPageView, dataPageView, learnMorePageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
-    path("data/", dataPageView, name="data")
+    path("data/", dataPageView, name="data"),
+    path("learnmore/", learnMorePageView, name="Learn More"),
 ]
