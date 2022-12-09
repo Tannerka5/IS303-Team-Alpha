11643bfb0d3cce0587cb6bbd879e84180f137af8
zachedge <103464298+zachedge@users.noreply.github.com>
2022-12-08 22:45:56 -0700

    Update urls.py

--- missingpersons/missingpersonsapp/urls.py
+++ missingpersons/missingpersonsapp/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("", indexPageView, name="index"),
     path("data/", dataPageView, name="data"),
     path("learnmore/", learnMorePageView, name="Learn More"),
-    path("search/", searchPageView, name="search")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
