a428993c02c20487f25fe3a529f995473df7b6df
zachedge <103464298+zachedge@users.noreply.github.com>
2022-12-08 22:44:16 -0700

    Update urls.py

--- missingpersons/missingpersonsapp/urls.py
+++ missingpersons/missingpersonsapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from .views import indexPageView, dataPageView, learnMorePageView
+from .views import indexPageView, dataPageView, learnMorePageView, searchPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("data/", dataPageView, name="data"),
     path("learnmore/", learnMorePageView, name="Learn More"),
+    path("search/", searchPageView, name="search")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
